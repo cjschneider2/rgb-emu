4a3b4d982adf32a1c3cc2aacab33847454aade90
Charles J. Schneider <cjschneider2@gmail.com>
2016-03-17 13:45:13 +0100

    fill out the read byte/word functions for the MMU
    (As much as possible anyways)

--- src/mmu.rs
+++ src/mmu.rs
@@ -1,22 +1,175 @@
+use std::fs::File;
+use std::io::Read;
+
+use z80::Z80;
 
 pub struct MMU {
-    foo: f32
+    bios: [u8; 256],
+    rom: Vec<u8>,
+    cartridge_type: Option<u8>,
+    mbc: u32, // TODO: ???
+    rom_offset: u16,
+    ram_offset: u16,
+    eram: Vec<u8>,
+    wram: Vec<u8>,
+    zram: Vec<u8>,
+    in_bios: bool,
+    in_e: bool, // TODO: ???
+    int_flag: bool,
 }
 
 impl MMU {
-    pub fn rb(&self, addr: u16) -> u8 {
+    pub fn new() -> MMU {
+        MMU {
+        bios: [
+                0x31, 0xFE, 0xFF, 0xAF, 0x21, 0xFF, 0x9F, 0x32,
+                0xCB, 0x7C, 0x20, 0xFB, 0x21, 0x26, 0xFF, 0x0E,
+                0x11, 0x3E, 0x80, 0x32, 0xE2, 0x0C, 0x3E, 0xF3,
+                0xE2, 0x32, 0x3E, 0x77, 0x77, 0x3E, 0xFC, 0xE0,
+                0x47, 0x11, 0x04, 0x01, 0x21, 0x10, 0x80, 0x1A,
+                0xCD, 0x95, 0x00, 0xCD, 0x96, 0x00, 0x13, 0x7B,
+                0xFE, 0x34, 0x20, 0xF3, 0x11, 0xD8, 0x00, 0x06,
+                0x08, 0x1A, 0x13, 0x22, 0x23, 0x05, 0x20, 0xF9,
+                0x3E, 0x19, 0xEA, 0x10, 0x99, 0x21, 0x2F, 0x99,
+                0x0E, 0x0C, 0x3D, 0x28, 0x08, 0x32, 0x0D, 0x20,
+                0xF9, 0x2E, 0x0F, 0x18, 0xF3, 0x67, 0x3E, 0x64,
+                0x57, 0xE0, 0x42, 0x3E, 0x91, 0xE0, 0x40, 0x04,
+                0x1E, 0x02, 0x0E, 0x0C, 0xF0, 0x44, 0xFE, 0x90,
+                0x20, 0xFA, 0x0D, 0x20, 0xF7, 0x1D, 0x20, 0xF2,
+                0x0E, 0x13, 0x24, 0x7C, 0x1E, 0x83, 0xFE, 0x62,
+                0x28, 0x06, 0x1E, 0xC1, 0xFE, 0x64, 0x20, 0x06,
+                0x7B, 0xE2, 0x0C, 0x3E, 0x87, 0xF2, 0xF0, 0x42,
+                0x90, 0xE0, 0x42, 0x15, 0x20, 0xD2, 0x05, 0x20,
+                0x4F, 0x16, 0x20, 0x18, 0xCB, 0x4F, 0x06, 0x04,
+                0xC5, 0xCB, 0x11, 0x17, 0xC1, 0xCB, 0x11, 0x17,
+                0x05, 0x20, 0xF5, 0x22, 0x23, 0x22, 0x23, 0xC9,
+                0xCE, 0xED, 0x66, 0x66, 0xCC, 0x0D, 0x00, 0x0B,
+                0x03, 0x73, 0x00, 0x83, 0x00, 0x0C, 0x00, 0x0D,
+                0x00, 0x08, 0x11, 0x1F, 0x88, 0x89, 0x00, 0x0E,
+                0xDC, 0xCC, 0x6E, 0xE6, 0xDD, 0xDD, 0xD9, 0x99,
+                0xBB, 0xBB, 0x67, 0x63, 0x6E, 0x0E, 0xEC, 0xCC,
+                0xDD, 0xDC, 0x99, 0x9F, 0xBB, 0xB9, 0x33, 0x3E,
+                0x3c, 0x42, 0xB9, 0xA5, 0xB9, 0xA5, 0x42, 0x4C,
+                0x21, 0x04, 0x01, 0x11, 0xA8, 0x00, 0x1A, 0x13,
+                0xBE, 0x20, 0xFE, 0x23, 0x7D, 0xFE, 0x34, 0x20,
+                0xF5, 0x06, 0x19, 0x78, 0x86, 0x23, 0x05, 0x20,
+                0xFB, 0x86, 0x20, 0xFE, 0x3E, 0x01, 0xE0, 0x50
+              ],
+            rom: Vec::<u8>::new(),
+            cartridge_type: None,
+            mbc: 0, // TODO: ???
+            rom_offset: 0x4000,
+            ram_offset: 0x0,
+            eram: Vec::<u8>::new(),
+            wram: Vec::<u8>::new(),
+            zram: Vec::<u8>::new(),
+            in_bios: true,
+            in_e: false,
+            int_flag: false,
+        }
+    }
+
+    pub fn load(&mut self, file: File) {
+        self.rom = file.bytes()
+                       .map(|r: Result<u8, _>| r.unwrap())
+                       .collect();
+        self.cartridge_type = match self.rom.get(0x0147) {
+            Some(val) => Some(*val),
+            None => None,
+        }
+    }
+
+    pub fn rb(&mut self, cpu: &Z80, addr: u16) -> u8 {
+        match addr & 0xF000 {
+            // Rom bank 0
+            0x0000 => {
+                if self.in_bios {
+                    if addr < 0x0100 {
+                        return self.bios[addr as usize];
+                    } else if cpu.get_pc() == 0x0100 {
+                        self.in_bios = false;
+                    }
+                } else {
+                    return *self.rom.get(addr as usize).unwrap();
+                }
+            },
+            0x1000 ... 0x3000 => {
+                return *self.rom.get(addr as usize).unwrap();
+            },
+            // Rom bank 1
+            0x4000 ... 0x7000 => {
+                let offset = self.rom_offset as usize + (addr & 0x3FFF) as usize;
+                return *self.rom.get(offset).unwrap();
+            },
+            // Video RAM
+            0x8000 ... 0x9000 => {
+                //return *GPU.vram.get((addr & 0x1FFF) as usize).unwrap();
+                unimplemented!();
+            },
+            // External RAM
+            0xA000 ... 0xB000 => {
+                let offset = self.ram_offset as usize + (addr & 0x1FFF) as usize;
+                return *self.eram.get(offset).unwrap();
+            },
+            // Work RAM & Echo
+            0xC000 ... 0xE000 => {
+                return *self.wram.get((addr & 0x1FFF) as usize).unwrap();
+            },
+            // Everything Else
+            0xF000 => {
+                match addr & 0x0F00 {
+                    // Echo RAM
+                    0x000 ... 0xD00 => {
+                        return *self.wram.get((addr & 0x1FFF) as usize).unwrap();
+                    },
+                    // OAM
+                    0xE00 => {
+                        unimplemented!();
+                    },
+                    // Zero-page RAM, I/O, Interrupts
+                    0xF00 => {
+                        if addr == 0xFFFF {
+                            return self.in_e as u8;
+                        } else if addr > 0xFF7F {
+                            return *self.zram.get((addr & 0x7F) as usize).unwrap();
+                        }
+                        match addr & 0xF0 {
+                            0x00 => {
+                                match addr & 0xF {
+                                    // Directional Pad
+                                    0x0 => { unimplemented!() },
+                                    // TODO: Timer flags?
+                                    0x4 ... 0x7 => { unimplemented!() },
+                                    0x15 => { return self.int_flag as u8; },
+                                    _ => { return 0u8; },
+                                }
+                            }
+                            0x10 => { return 0u8; },
+                            // GPU read-byte
+                            0x40 ... 0x70 => { unreachable!()},
+                            _ => {unreachable!()},
+                        }
+                    },
+                    _ => {unreachable!()},
+                }
+            },
+            _ => {unreachable!()},
+        }
         // read 8-bit byte from memory
         unimplemented!();
     }
-    pub fn rw(&self, addr: u16) -> u16 {
+    pub fn rw(&mut self, cpu: &Z80, addr: u16) -> u16 {
         // read 16-bit word from memory
-        unimplemented!();
+        let mut val = 0u16;
+        val += self.rb(cpu, addr) as u16;
+        val += (self.rb(cpu, addr+1) as u16) << 8;
+        val
     }
-    pub fn wb(&self, addr: u16, val: u8) {
+    pub fn wb(&self, cpu: &Z80, addr: u16, val: u8) {
         // write 8-bit byte from memory
         unimplemented!();
     }
-    pub fn ww(&self, addr: u16, val: u16) {
+    pub fn ww(&self, cpu: &Z80, addr: u16, val: u16) {
         // write 16-bit word from memory
         unimplemented!();
     }
--- src/z80.rs
+++ src/z80.rs
@@ -4,7 +4,9 @@ use mmu::MMU;
 // really want to worry about roll-over operations in rust
 // TODO: Actually... Change back to 8 bit and use the language checks; they are
 // actually really useful. So change back to u8 eventually.
-struct Z80 {
+pub struct Z80 {
+    halt:    bool,
+    stop:    bool,
     clock_m: f32,
     clock_t: f32,
     reg_a:   u8,
@@ -19,11 +21,14 @@ struct Z80 {
     reg_sp:  u16,
     reg_m:   i16,
     reg_t:   i16,
+    reg_ime: u16,
 }
 
 impl Z80 {
     pub fn new() -> Z80 {
         Z80 {
+            halt:    false,
+            stop:    false,
             clock_m: 0.0,
             clock_t: 0.0,
             reg_a:   0,
@@ -38,26 +43,117 @@ impl Z80 {
             reg_sp:  0,
             reg_m:   0,
             reg_t:   0,
+            reg_ime: 0,
         }
     }
 
+    pub fn get_pc(&self) -> u16 { self.reg_pc }
+
     /// Resets processor state
     pub fn reset(&mut self) {
+        self.halt = false; self.stop = false;
         self.clock_m = 0.0; self.clock_t = 0.0;
         self.reg_a =  0; self.reg_b =  0; self.reg_c =  0;
         self.reg_d =  0; self.reg_e =  0; self.reg_h =  0;
         self.reg_l =  0; self.reg_f =  0; self.reg_pc = 0;
         self.reg_sp = 0; self.reg_m =  0; self.reg_t =  0;
+        self.reg_ime = 0;
+    }
+
+    // Load / Store
+    pub fn ldrr_bb(&mut self) { self.reg_b = self.reg_b; self.reg_m = 1; self.reg_t = 4;}
+    pub fn ldrr_bc(&mut self) { self.reg_b = self.reg_c; self.reg_m = 1; self.reg_t = 4;}
+    pub fn ldrr_bd(&mut self) { self.reg_b = self.reg_d; self.reg_m = 1; self.reg_t = 4;}
+    pub fn ldrr_be(&mut self) { self.reg_b = self.reg_e; self.reg_m = 1; self.reg_t = 4;}
+    pub fn ldrr_bh(&mut self) { self.reg_b = self.reg_h; self.reg_m = 1; self.reg_t = 4;}
+    pub fn ldrr_bl(&mut self) { self.reg_b = self.reg_l; self.reg_m = 1; self.reg_t = 4;}
+    pub fn ldrr_ba(&mut self) { self.reg_b = self.reg_a; self.reg_m = 1; self.reg_t = 4;}
+    pub fn ldrr_cb(&mut self) { self.reg_c = self.reg_b; self.reg_m = 1; self.reg_t = 4;}
+    pub fn ldrr_cc(&mut self) { self.reg_c = self.reg_c; self.reg_m = 1; self.reg_t = 4;}
+    pub fn ldrr_cd(&mut self) { self.reg_c = self.reg_d; self.reg_m = 1; self.reg_t = 4;}
+    pub fn ldrr_ce(&mut self) { self.reg_c = self.reg_e; self.reg_m = 1; self.reg_t = 4;}
+    pub fn ldrr_ch(&mut self) { self.reg_c = self.reg_h; self.reg_m = 1; self.reg_t = 4;}
+    pub fn ldrr_cl(&mut self) { self.reg_c = self.reg_l; self.reg_m = 1; self.reg_t = 4;}
+    pub fn ldrr_ca(&mut self) { self.reg_c = self.reg_a; self.reg_m = 1; self.reg_t = 4;}
+    pub fn ldrr_db(&mut self) { self.reg_d = self.reg_b; self.reg_m = 1; self.reg_t = 4;}
+    pub fn ldrr_dc(&mut self) { self.reg_d = self.reg_c; self.reg_m = 1; self.reg_t = 4;}
+    pub fn ldrr_dd(&mut self) { self.reg_d = self.reg_d; self.reg_m = 1; self.reg_t = 4;}
+    pub fn ldrr_de(&mut self) { self.reg_d = self.reg_e; self.reg_m = 1; self.reg_t = 4;}
+    pub fn ldrr_dh(&mut self) { self.reg_d = self.reg_h; self.reg_m = 1; self.reg_t = 4;}
+    pub fn ldrr_dl(&mut self) { self.reg_d = self.reg_l; self.reg_m = 1; self.reg_t = 4;}
+    pub fn ldrr_da(&mut self) { self.reg_d = self.reg_a; self.reg_m = 1; self.reg_t = 4;}
+    pub fn ldrr_eb(&mut self) { self.reg_e = self.reg_b; self.reg_m = 1; self.reg_t = 4;}
+    pub fn ldrr_ec(&mut self) { self.reg_e = self.reg_c; self.reg_m = 1; self.reg_t = 4;}
+    pub fn ldrr_ed(&mut self) { self.reg_e = self.reg_d; self.reg_m = 1; self.reg_t = 4;}
+    pub fn ldrr_ee(&mut self) { self.reg_e = self.reg_e; self.reg_m = 1; self.reg_t = 4;}
+    pub fn ldrr_eh(&mut self) { self.reg_e = self.reg_h; self.reg_m = 1; self.reg_t = 4;}
+    pub fn ldrr_el(&mut self) { self.reg_e = self.reg_l; self.reg_m = 1; self.reg_t = 4;}
+    pub fn ldrr_ea(&mut self) { self.reg_e = self.reg_a; self.reg_m = 1; self.reg_t = 4;}
+    pub fn ldrr_hb(&mut self) { self.reg_h = self.reg_b; self.reg_m = 1; self.reg_t = 4;}
+    pub fn ldrr_hc(&mut self) { self.reg_h = self.reg_c; self.reg_m = 1; self.reg_t = 4;}
+    pub fn ldrr_hd(&mut self) { self.reg_h = self.reg_d; self.reg_m = 1; self.reg_t = 4;}
+    pub fn ldrr_he(&mut self) { self.reg_h = self.reg_e; self.reg_m = 1; self.reg_t = 4;}
+    pub fn ldrr_hh(&mut self) { self.reg_h = self.reg_h; self.reg_m = 1; self.reg_t = 4;}
+    pub fn ldrr_hl(&mut self) { self.reg_h = self.reg_l; self.reg_m = 1; self.reg_t = 4;}
+    pub fn ldrr_ha(&mut self) { self.reg_h = self.reg_a; self.reg_m = 1; self.reg_t = 4;}
+    pub fn ldrr_lb(&mut self) { self.reg_l = self.reg_b; self.reg_m = 1; self.reg_t = 4;}
+    pub fn ldrr_lc(&mut self) { self.reg_l = self.reg_c; self.reg_m = 1; self.reg_t = 4;}
+    pub fn ldrr_ld(&mut self) { self.reg_l = self.reg_d; self.reg_m = 1; self.reg_t = 4;}
+    pub fn ldrr_le(&mut self) { self.reg_l = self.reg_e; self.reg_m = 1; self.reg_t = 4;}
+    pub fn ldrr_lh(&mut self) { self.reg_l = self.reg_h; self.reg_m = 1; self.reg_t = 4;}
+    pub fn ldrr_ll(&mut self) { self.reg_l = self.reg_l; self.reg_m = 1; self.reg_t = 4;}
+    pub fn ldrr_la(&mut self) { self.reg_l = self.reg_a; self.reg_m = 1; self.reg_t = 4;}
+    pub fn ldrr_ab(&mut self) { self.reg_a = self.reg_b; self.reg_m = 1; self.reg_t = 4;}
+    pub fn ldrr_ac(&mut self) { self.reg_a = self.reg_c; self.reg_m = 1; self.reg_t = 4;}
+    pub fn ldrr_ad(&mut self) { self.reg_a = self.reg_d; self.reg_m = 1; self.reg_t = 4;}
+    pub fn ldrr_ae(&mut self) { self.reg_a = self.reg_e; self.reg_m = 1; self.reg_t = 4;}
+    pub fn ldrr_ah(&mut self) { self.reg_a = self.reg_h; self.reg_m = 1; self.reg_t = 4;}
+    pub fn ldrr_al(&mut self) { self.reg_a = self.reg_l; self.reg_m = 1; self.reg_t = 4;}
+    pub fn ldrr_aa(&mut self) { self.reg_a = self.reg_a; self.reg_m = 1; self.reg_t = 4;}
+
+    pub fn LDrHLm_b(&mut self, mmu: &mut MMU ) {
+        let addr:u16 = ((self.reg_h as u16) << 8u16) + self.reg_l as u16;
+        self.reg_b = mmu.rb(&self, addr);
+        self.reg_m=2; self.reg_t=8;
+    }
+    pub fn LDrHLm_c(&mut self, mmu: &mut MMU ) {
+        let addr:u16 = ((self.reg_h as u16) << 8u16) + self.reg_l as u16;
+        self.reg_c = mmu.rb(&self, addr);
+        self.reg_m=2; self.reg_t=8;
+    }
+    pub fn LDrHLm_d(&mut self, mmu: &mut MMU ) {
+        let addr:u16 = ((self.reg_h as u16) << 8u16) + self.reg_l as u16;
+        self.reg_d = mmu.rb(&self, addr);
+        self.reg_m=2; self.reg_t=8;
+    }
+    pub fn LDrHLm_e(&mut self, mmu: &mut MMU ) {
+        let addr:u16 = ((self.reg_h as u16) << 8u16) + self.reg_l as u16;
+        self.reg_e = mmu.rb(&self, addr);
+        self.reg_m=2; self.reg_t=8;
+    }
+    pub fn LDrHLm_h(&mut self, mmu: &mut MMU ) {
+        let addr:u16 = ((self.reg_h as u16) << 8u16) + self.reg_l as u16;
+        self.reg_h = mmu.rb(&self, addr);
+        self.reg_m=2; self.reg_t=8;
+    }
+    pub fn LDrHLm_l(&mut self, mmu: &mut MMU ) {
+        let addr:u16 = ((self.reg_h as u16) << 8u16) + self.reg_l as u16;
+        self.reg_l = mmu.rb(&self, addr);
+        self.reg_m=2; self.reg_t=8;
+    }
+    pub fn LDrHLm_a(&mut self, mmu: &mut MMU ) {
+        let addr:u16 = ((self.reg_h as u16) << 8u16) + self.reg_l as u16;
+        self.reg_a = mmu.rb(&self, addr);
+        self.reg_m=2; self.reg_t=8;
     }
 
     /// (ADD A, E): Add reg_e to reg_a, result in reg_a
     pub fn addr_e(&mut self) {
-        self.reg_a += self.reg_e; // add operation
-        self.reg_f = 0; // clear flags
-        if (self.reg_a & 0xFF) == 0 { self.reg_f |= 0x80; } // check for zero
-        if self.reg_a > 0xFF { self.reg_f |= 0x10; } // check for carry
-        self.reg_a &= 0xFF; // mask to 8-bit
-        self.reg_m = 1; self.reg_t = 4;// 1 M-time taken
+        self.reg_a += self.reg_e; self.reg_f = 0;
+        if (self.reg_a & 0xFF) == 0 { self.reg_f |= 0x80; }
+        if self.reg_a > 0xFF { self.reg_f |= 0x10; }
+        self.reg_a &= 0xFF;
+        self.reg_m = 1; self.reg_t = 4;
     }
 
     /// (CP A, B): Compare reg_b to reg_a, setting flags
@@ -78,28 +174,28 @@ impl Z80 {
     // memory handling instructions
 
     /// (PUSH BC): Push reg_b and reg_c onto the stack
-    pub fn push_bc(&mut self, mmu: MMU) {
+    pub fn push_bc(&mut self, mmu: &mut MMU) {
         self.reg_sp -= 1; // decrement stack pointer
-        mmu.wb(self.reg_sp, self.reg_b); // Write reg_b
+        mmu.wb(&self, self.reg_sp, self.reg_b); // Write reg_b
         self.reg_sp -= 1; // decrement stack pointer
-        mmu.wb(self.reg_sp, self.reg_c); // Write reg_c
+        mmu.wb(&self, self.reg_sp, self.reg_c); // Write reg_c
         self.reg_m = 3; self.reg_t = 12; // 3 M-times taken
     }
 
     /// (POP HL): Pop reg_h and reg_l off of the stack
-    pub fn pop_hl(&mut self, mmu: MMU) {
-        self.reg_l = mmu.rb(self.reg_sp); // read reg_l
+    pub fn pop_hl(&mut self, mmu: &mut MMU) {
+        self.reg_l = mmu.rb(&self, self.reg_sp); // read reg_l
         self.reg_sp += 1; // increment stack pointer
-        self.reg_h = mmu.rb(self.reg_sp); // read reg_h
+        self.reg_h = mmu.rb(&self, self.reg_sp); // read reg_h
         self.reg_sp += 1; // increment stack pointer
         self.reg_m = 3; self.reg_t = 12; // 3 M-times taken
     }
 
     /// (LD A, Addr): Read a byte from an absolute address into reg_a
-    pub fn ld_amm(&mut self, mmu: MMU) {
-        let addr = mmu.rw(self.reg_pc); // get address from instruction
+    pub fn ld_amm(&mut self, mmu: &mut MMU) {
+        let addr = mmu.rw(&self, self.reg_pc); // get address from instruction
         self.reg_pc += 2; // increment program counter
-        self.reg_a = mmu.rb(addr); // read from the address
+        self.reg_a = mmu.rb(&self, addr); // read from the address
         self.reg_m = 4; self.reg_t = 16; // 4 M-times taken
     }
 }
